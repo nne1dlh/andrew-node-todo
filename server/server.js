8acf59eba0e3a47d2991762efd7a22bb73f66de4
nne1dlh <dharder@maine.rr.com>
2019-03-02 14:49:51 -0500

    add test databse envirnmoent cvariable

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env *******', env);
+
+if(env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 const xpress = require('express');
 const _ = require('lodash');
 const bp = require('body-parser');
